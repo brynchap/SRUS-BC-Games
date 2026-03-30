7f7e3cf68816036824aa948694a3ab22d4fc17ef
Bryn Chapman <20143871@tafe.wa.edu.au>
2026-03-30 13:42:32 +0800

    fix: add forgotten indent to PlayerBST class

--- app/player_bst.py
+++ app/player_bst.py
@@ -2,6 +2,6 @@ class PlayerBST:
     def __init__(self):
         self._root = None
 
-@property
-def root(self):
-    return self._root
+    @property
+    def root(self):
+        return self._root
