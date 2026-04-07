a90f5e2b78b98795d3032a3bd2c6458dd71a108e
brynchap <brynchapman01@gmail.com>
2026-04-07 15:42:15 +0800

    Feat: Add BST Search() method

--- app/player_bst.py
+++ app/player_bst.py
@@ -13,16 +13,27 @@ class PlayerBST:
         key = player.name
         # If the tree is empty, return a new node
         if root is None:
-            return PlayerBNode(key)
+            return PlayerBNode(player)
         # Otherwise, recur down the tree
-        if key == root.player: # Handle Duplicates by overwriting them but keeping the old left and right values
-            node = PlayerBNode(key)
+        if key == root.player.name: # Handle Duplicates by overwriting them but keeping the old left and right values
+            node = PlayerBNode(player)
             node.left = root.left
             node.right = root.right
             return node
-        if key < root.player: #left
+        if key < root.player.name: #left
             root.left = self.insert(root.left, player)
         else: # right
             root.right = self.insert(root.right, player)
         # Return the (unchanged) node pointer
         return root
+
+    def search(self, root, name):
+        key = name
+        if root is None:
+            return None
+        if key == root.player.name: # Handle Duplicates by overwriting them but keeping the old left and right values
+            return root
+        if key < root.player.name: #left
+            return self.search(root.left, name)
+        else: # right
+            return self.search(root.right, name)
--- test/player_bnode_bst_test.py
+++ test/player_bnode_bst_test.py
@@ -4,7 +4,7 @@ from player_bst import PlayerBST
 from player_bnode import PlayerBNode
 
 class MyTests(unittest.TestCase):
-    def test_custom(self):
+    def test_insert(self):
         tree = PlayerBST()
         my_player1 = Player("1", "Alex")
         my_player2 = Player("2", "Brian")
@@ -18,8 +18,30 @@ class MyTests(unittest.TestCase):
         tree.root = tree.insert(tree.root, my_player4)
         tree.root = tree.insert(tree.root, my_player5)
         tree.root = tree.insert(tree.root, my_player6)
-        self.assertEqual(tree.root.player, "Charlie")
-        self.assertEqual(tree.root.left.player, "Alex")
-        self.assertEqual(tree.root.left.right.player, "Brian")
-        self.assertEqual(tree.root.right.player, "Devon")
-        self.assertEqual(tree.root.right.right.player, "Eric")
+        self.assertEqual(tree.root.player.name, "Charlie")
+        self.assertEqual(tree.root.left.player.name, "Alex")
+        self.assertEqual(tree.root.left.right.player.name, "Brian")
+        self.assertEqual(tree.root.right.player.name, "Devon")
+        self.assertEqual(tree.root.right.right.player.name, "Eric")
+
+    def test_search(self):
+        tree = PlayerBST()
+        my_player1 = Player("1", "Alex")
+        my_player2 = Player("2", "Brian")
+        my_player3 = Player("3", "Charlie")
+        my_player4 = Player("4", "Devon")
+        my_player5 = Player("5", "Alex")
+        my_player6 = Player("6", "Eric")
+        tree.root = tree.insert(tree.root, my_player3)
+        tree.root = tree.insert(tree.root, my_player1)
+        tree.root = tree.insert(tree.root, my_player2)
+        tree.root = tree.insert(tree.root, my_player4)
+        tree.root = tree.insert(tree.root, my_player5)
+        tree.root = tree.insert(tree.root, my_player6)
+        tree.search(tree.root, "Alex")
+        self.assertEqual("Alex", tree.search(tree.root, "Alex").player.name)
+        self.assertEqual("5", tree.search(tree.root, "Alex").player.uid)
+        self.assertEqual("Charlie", tree.search(tree.root, "Charlie").player.name)
+        self.assertEqual("3", tree.search(tree.root, "Charlie").player.uid)
+        self.assertEqual("Eric", tree.search(tree.root, "Eric").player.name)
+        self.assertEqual("6", tree.search(tree.root, "Eric").player.uid)
